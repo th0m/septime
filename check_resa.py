cf2b62f5badc1e81c93b3519e574820d5f46a2ff
Thomas Lefebvre <thomas.lefebvre3@gmail.com>
2016-01-19 17:57:20 +0100

    Initial commit of the availability checking script for Septime

---
+++ check_resa.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import json
+import requests
+r = requests.get('https://module.lafourchette.com/fr_FR/resa/pick-date/10889-d34ca/54499')
+c = json.loads(r.text)
+availableDateList = c['availableDateList']
+for availableDate in availableDateList:
+    r = requests.get('https://module.lafourchette.com/fr_FR/resa/pick-time/{}/10889-d34ca/54499'.format(availableDate['date']))
+    c = json.loads(r.text)
+    availableTimeslotList = c['availableTimeslotList']
+    for availableTimeslot in availableTimeslotList:
+        if availableTimeslot['name'] == 'DINER' and availableTimeslot['timeslots']:
+            print(availableDate['date'])
+            for timeslot in availableTimeslot['timeslots']:
+                print(timeslot['time'])
